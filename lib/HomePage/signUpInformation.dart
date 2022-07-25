90ac03e16d7a023000b26bcd18d923a7d42a562e
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2022-07-25 20:42:44 +0300

    Back end

--- lib/HomePage/signUpInformation.dart
+++ lib/HomePage/signUpInformation.dart
@@ -28,11 +28,7 @@ class _afterSignupState extends State<afterSignup> {
         })));
       } else if (radioChoiceValue == "Customer") {
         Navigator.of(context).push(MaterialPageRoute(builder: ((context) {
-<<<<<<< HEAD
           return afterChooseCustomer(widget.userID);
-=======
-          return afterChooseCustomer();
->>>>>>> parent of a40c14f (Back end)
         })));
       }
     } else {
