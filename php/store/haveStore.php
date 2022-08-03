3846efb610bb1c501b4c12fa2858e17f866b1ab5
Raed-Khwayreh <raedkh.2000@gmail.com>
2022-08-03 14:13:00 +0300

    Update haveStore.php

--- php/store/haveStore.php
+++ php/store/haveStore.php
@@ -5,7 +5,7 @@ $stmt = $con->prepare("UPDATE `users` SET `have_store`= ? WHERE `id` = ?");
 $stmt->execute(array("yes", $id));
 $count = $stmt->rowCount();
 $stmt2 = $con->prepare("INSERT INTO `store`(`storeID`, `storeName`, `storeImage`, `earrings`) VALUES (?,?,?,?)");
-$stmt2->execute(array($id, "Store Name"," ",0));
+$stmt2->execute(array($id, "Store Name","",0));
 $count2= $stmt2->rowCount();
 if ($count > 0) {
     echo json_encode(array("status" => "suc"));
